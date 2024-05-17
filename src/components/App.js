7f34fc33f322bd9dfcbb1a7efa63140318647117
Cole Elsasser <elsascol@uw.edu>
2024-05-17 00:02:51 -0700

    Update app for firebase display

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import '../index.css';
 import { Quiz } from './Quiz';
 import {Homepage} from './Homepage'
+import { Footer1 } from './Footer';
+import { NavbarBrand } from 'react-bootstrap';
 // Cleaned
 
 export default function App(props){
     return (
-    <Quiz />);
+    <div>
+    <NavbarBrand />
+    <Quiz />
+    <Footer1 />  
+    </div>  
+    );
 }
